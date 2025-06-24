0c840102e89655230288ff4dd264dcdbb7d8cee3
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-24 11:44:28 +0200

    Add middleware to check auth for editing/deleting comment

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -25,6 +25,41 @@ async function checkPostOwnership(req, res, next) {
     }
 }
 
+async function checkCommentAuth(req, res, next) {
+    try {
+        const comment = await prisma.comment.findUnique({
+            where: {
+                id: req.params.commentId,
+            },
+            select: {
+                authorId: true,
+                post: {
+                    select: {
+                        authorId: true,
+                    },
+                },
+            },
+        });
+        if (comment === null) {
+            res.status(404).send("No comment found");
+            return;
+        }
+        // Check if current user is either comment author or post author
+        if (
+            comment.authorId === req.user.id ||
+            comment.post.authorId === req.user.id
+        ) {
+            next();
+        } else {
+            res.status(403).send("Not authorized to edit this resource");
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Internal server error");
+    }
+}
+
 module.exports = {
     checkPostOwnership,
-}
+    checkCommentAuth,
+};
--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -1,8 +1,9 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("../controllers/commentsController");
+const auth = require("../middleware/auth");
 
 router.post("/", controller.createComment);
 
-router.delete("/:commentId", controller.deleteComment);
+router.delete("/:commentId", auth.checkCommentAuth, controller.deleteComment);
 
 module.exports = router;
