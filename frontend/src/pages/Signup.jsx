58f04ad09540f4c3316d77f453426d1b149f6bba
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-24 15:26:58 +0200

    Add signup page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const API_URL = import.meta.env.VITE_API_URL;
+import styles from "./forms.module.css";
+
+function Signup() {
+    const [errorMessages, setErrorMessages] = useState([]);
+    const navigate = useNavigate();
+
+    async function handleSignup(e) {
+        try {
+            e.preventDefault();
+
+            const formData = new FormData(e.target);
+            const data = Object.fromEntries(formData);
+
+            const res = await fetch(`${API_URL}/auth/sign-up`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "Application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            // If sucessful, attempt to login user
+            if (res.status === 200) {
+                const loginRes = await fetch(`${API_URL}/auth/log-in`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "Application/json",
+                    },
+                    body: JSON.stringify({
+                        email: data.email,
+                        password: data.password,
+                    }),
+                });
+                if (loginRes.status === 200) {
+                    const resPayload = await loginRes.json();
+                    localStorage.setItem("jwt-token", resPayload.token);
+                    navigate("/");
+                } else {
+                    navigate("/login");
+                }
+            // If unsuccessful display validation error messages
+            } else if (res.status === 400) {
+                const payload = await res.json();
+                setErrorMessages(payload.errors);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <>
+            <ul className={styles.errorList}>
+                {errorMessages.map((error, index) => {
+                    return (
+                        <li className={styles.errorMessage} key={index}>
+                            {error.msg}
+                        </li>
+                    );
+                })}
+            </ul>
+            <form className={styles.form} onSubmit={handleSignup}>
+                <legend>Sign Up</legend>
+                <label htmlFor="username">Username: </label>
+                <input type="text" name="username" id="username" required />
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="email" id="email" required />
+                <label htmlFor="password">Password: </label>
+                <input type="password" name="password" id="password" required />
+                <label htmlFor="confPassword">Confirm Password: </label>
+                <input type="password" name="confPassword" id="confPassword" required />
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    );
+}
+
+export default Signup;
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,10 +1,15 @@
 import App from "./App";
+import Signup from "./pages/Signup";
 
 const routes = [
     {
         path: "/",
         element: <App />,
-    }
+    },
+    {
+        path: "signup",
+        element: <Signup />,
+    },
 ];
 
 export default routes;
