e88f489bcae0f5c5125bdeb36348647f3a88c01e
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-25 15:29:11 +0200

    Add button and function for following/unfollowing

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,13 +1,33 @@
 import styles from "./profile.module.css";
 import userIcon from "../../assets/user-icon.png";
+import { useState } from "react";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Profile({ user }) {
+    const [following, setFollowing] = useState(user.following);
+
+    async function toggleFollow() {
+        const res = await fetch(`${API_URL}/user/follow/${user.username}`, {
+            // DELETE to unfollow, POST to follow
+            method: user.following ? "DELETE" : "POST",
+            headers: {
+                authorization: localStorage.getItem("jwt-token"),
+            },
+        });
+        if (res.status === 200) {
+            setFollowing(!following);
+        }
+    }
+
     return (
         <div className={styles.profileContainer}>
             <div className={styles.profileUpper}>
                 <img src={user.imgUrl ? user.imgUrl : userIcon} alt="User icon" />
                 <div>
-                    <h2>{user.username}</h2>
+                    <div>
+                        <h2>{user.username}</h2>
+                        <button className={styles.followBtn} onClick={toggleFollow}>{following ? "Unfollow" : "Follow"}</button>
+                    </div>
                     <p>{user.desc}</p>
                 </div>
             </div>
