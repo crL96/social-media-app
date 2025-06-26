e1c9f3214c6415927c4d4809796d546a567e055c
crL96 <190506813+crL96@users.noreply.github.com>
2025-06-26 16:57:02 +0200

    Add delete button to users own comments

--- frontend/src/components/comment/Comment.jsx
+++ frontend/src/components/comment/Comment.jsx
@@ -1,10 +1,34 @@
 import styles from "./comment.module.css";
 import userIcon from "../../assets/user-icon.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Comment({ data }) {
     const formattedTime = new Date(data.timestamp).toLocaleString("en-GB");
-    
+    const ownerStatus = localStorage.getItem("currentUser") === data.author.username;
+    const [deleted, setDeleted] = useState(false);
+
+    async function handleDelete() {
+        try {
+            const res = await fetch(`${API_URL}/posts/postId/comments/${data.id}`, {
+                method: "DELETE",
+                headers: {
+                    authorization: localStorage.getItem("jwt-token"),
+                },
+            });
+            if (res.status === 200) {
+                setDeleted(true);
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // Enables removing the comment after deletion without fetching new data
+    // Fully removed with next data update/fetch
+    if (deleted) return null;
+
     return (
         <div className={[styles.comment, "comment"].join(" ")}>
             <div className={styles.header}>
@@ -12,6 +36,11 @@ function Comment({ data }) {
                 <Link className="profileLink" to={`/profile/${data.author.username}`}>
                     <h4>{data.author.username}</h4>
                 </Link>
+                {ownerStatus ? (
+                    <button className={styles.deleteBtn} onClick={handleDelete}>
+                        Delete
+                    </button>
+                ): null}
             </div>
             <p>{data.text}</p>
             <p>{formattedTime}</p>
